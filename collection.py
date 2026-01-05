7cb9d7695b84b5c55f5cedc57c27c52080edfabf
kanikaaaa99999 <kooslovv@gmail.com>
2026-01-05 22:54:42 +0530

    basics

--- collection.py
+++ collection.py
@@ -1,6 +1,10 @@
 #collection = single variable used tostore multiple values
-# list , tuple , set , dictionary
+# list [] , tuple () , set {} , dictionary {key:value}
 #list = ordered , changeable , allows duplicate values
+#set = unordered , unindexed , no duplicate values
+#tuple = ordered , unchangeable , allows duplicate values faster than list
+#dictionary = unordered , changeable , indexed , no duplicate keys
+
 
 
 fruits = ["apple", "banana", "mango", "grapes" , "orange" , "kiwi"]
@@ -10,4 +14,16 @@ fruits = ["apple", "banana", "mango", "grapes" , "orange" , "kiwi"]
  #   print(fruit)
 #print(dir(fruits))
 #print(help(fruits))
-print(len(fruits))
+#print(len(fruits))
+#print ("pineapple" in fruits)
+#fruits[0] ="pineapple"
+#for fruit in fruits:
+     #print(fruit)
+
+#fruits.append("pineapple")
+#print(fruits)
+#fruits.remove("apple")
+fruits.sort()
+fruits.reverse()
+fruits.clear()
+print(fruits.index("mango"))
