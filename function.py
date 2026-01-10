4f66776593cd2691a856af8432d5433091dfcccf
kanikaaaa99999 <kooslovv@gmail.com>
2026-01-10 23:16:43 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,13 @@
+
+#function is a block of reusable code that is used to perform a single action
+# place () after the function name to call the function
+
+def happy_birthday(name, age):
+    print(f"happy birthday to {name}")
+    print(f"you are {age} years old")
+    print("happy birthday to you")
+    print()
+
+happy_birthday("Alice", 20)
+happy_birthday("Bob", 25)
+happy_birthday("Charlie", 30)
