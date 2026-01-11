b8cf3e7c96438d949b50d8f6b97a600e1e21e917
kanikaaaa99999 <kooslovv@gmail.com>
2026-01-11 22:46:37 +0530

    Create arbitrary_arguments.py

---
+++ arbitrary_arguments.py
@@ -0,0 +1,7 @@
+
+# *args = allows you to pass multiple non-key arguements to a function
+#         *args is a tuple of all the arguments passed to the function
+#         **kwargs = allows you to pass multiple keyword arguments to a function
+#                    **kwargs is a dictionary of all the keyword arguments passed to the function
+
+
