feda947b05187024267e5a80a170b59e7bffcf0b
kanikaaaa99999 <kooslovv@gmail.com>
2026-01-16 21:53:45 +0530

    Create module.py

---
+++ module.py
@@ -0,0 +1,6 @@
+# module is a file containing code you want to include in your program
+# use 'import' to include a module (built in your program)
+# useful to break up a large program reusable seprate files
+
+#print(help("modules"))
+print(help("math"))
